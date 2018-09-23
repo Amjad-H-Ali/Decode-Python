598942146206ace0bfdbbe20d8967f445e5c7911
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-09-22 20:48:50 -0500

    Fixed a few bugs regarding len(); function return correct value

--- decode.py
+++ decode.py
@@ -8,7 +8,7 @@ def helper(data, k):
 		return 1;
 
 	# Variable holding value of first index of char in current string
-	s = data.length - k	
+	s = len(data) - k	
 
 	# When string contains char '0'; no way to decode
 	if (data[s] == '0'):
@@ -18,8 +18,10 @@ def helper(data, k):
 	result = helper(data, k - 1)
 
 	# Add helper("345") only if digit less than 27
-	if (k >= 2 a and int(data[s:s+2]) <= 26):
+	if (k >= 2  and int(data[s:s+2]) <= 26):
 		result += helper(data, k - 2)
+
+	return result	
  	
 
 
@@ -28,4 +30,11 @@ def helper(data, k):
 # Takes in digits in the form of a string type as Argument
 def num_of_ways(data):
 
-	return helper(data, data.length)
+	return helper(data, len(data))
+
+print(num_of_ways("123")) # 3
+print(num_of_ways("023")) # 0
+print(num_of_ways("1203")) # 1
+print(num_of_ways("")) # 1
+print(num_of_ways("2723")) # 2
+print(num_of_ways("12")) # 2
