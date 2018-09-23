8c80c08c1761349440301ff8befe984801342607
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-09-22 20:14:20 -0500

    Define two functions; One Takes in digits in the form of a string type as Argument; Second Uses recursion to evaluate the number of ways to decode data

--- decode.py
+++ decode.py
@@ -1,1 +1,12 @@
 print("Decode!")
+
+# Uses recursion to evaluate the number of ways to decode data 
+def helper(data, k):
+
+
+
+
+# Takes in digits in the form of a string type as Argument
+def num_of_ways(data):
+
+	return helper(data, data.length)
