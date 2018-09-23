97841cbc49d2d8c01fdfb2d9c92285bcd82a1865
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-09-22 20:18:56 -0500

    Exit code created

--- decode.py
+++ decode.py
@@ -1,7 +1,11 @@
 print("Decode!")
 
-# Uses recursion to evaluate the number of ways to decode data 
+# Uses recursion to evaluate the number of ways to decode data
+# k represents the portion of the string currently being evaluated
 def helper(data, k):
+	# Exit code: for empty strings ("")
+	if (k == 0):
+		return 1;
 
 
 
